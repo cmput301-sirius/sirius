4e267a19afadea2e9ed17115cd185a1891444cbe
03122005pri <rpolishchuk3@icloud.com>
2026-09-24 18:20:29 -0600

    Created a file with class 'Circle'

--- app/src/main/java/com/example/sirius/Shape.kt
+++ app/src/main/java/com/example/sirius/Shape.kt
@@ -3,8 +3,3 @@ package com.example.sirius
 abstract class Shape(var x: Int, var y: Int) {
     val color: String = "azure"
 }
-
-class Circle(x: Int, y: Int, var radius: Double) : Shape(x, y) {
-
-
-}
