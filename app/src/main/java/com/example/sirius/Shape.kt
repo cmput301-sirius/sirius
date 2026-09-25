74142ae9005e7a276cad8aa13fb74b8f2039c86a
03122005pri <rpolishchuk3@icloud.com>
2026-09-24 18:09:05 -0600

    Added colour 'azure'

--- app/src/main/java/com/example/sirius/Shape.kt
+++ app/src/main/java/com/example/sirius/Shape.kt
@@ -1,7 +1,7 @@
 package com.example.sirius
 
 abstract class Shape(var x: Int, var y: Int) {
-    
+    val color: String = "azure"
 }
 
 class Circle(x: Int, y: Int, var radius: Double) : Shape(x, y) {
